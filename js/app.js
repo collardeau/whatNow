c25d2ad9cf0d401ac520f832ba44300637a2b504
Thomas Collardeau <thomas@collardeau.com>
2014-10-09 00:27:48 +0200

    add new Activity modal (first pass)

--- js/app.js
+++ js/app.js
@@ -4,9 +4,39 @@
 
 var WhatNowApp = angular.module('whatNow', ['ionic', 'whatNow.services']);
 
-WhatNowApp.controller('WhatNowCtrl', function($scope, activityService) {
+WhatNowApp.controller('WhatNowCtrl', function($scope, activityService, $ionicModal) {
 
-    $scope.activites = activityService.activities;
+    $scope.activities = activityService.activities;
+    $scope.newActivity = {};
 
 
+   $ionicModal.fromTemplateUrl('new-activity.html', function(modal){
+       $scope.activityModal = modal;
+       },
+       {
+           scope: $scope,
+           animation: 'slide-in-up'
+
+        });
+
+    $scope.newActivity = function(){
+        $scope.activityModal.show();
+    };
+
+    $scope.closeNewActivity = function(){
+        $scope.activityModal.hide();
+    };
+
+   $scope.addActivity = function(){
+        var title = $scope.newActivity.title,
+            urgency = $scope.newActivity.urgency,
+            duration = $scope.newActivity.duration;
+       $scope.activities[title] = {
+           title : title,
+           urgency: urgency,
+           duration: duration
+       };
+       $scope.activityModal.hide();
+    };
 });
+
