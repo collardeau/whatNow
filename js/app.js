c497dfdef7fb80a83258391aca2c8ab426af86da
Thomas Collardeau <thomas@collardeau.com>
2014-10-29 18:08:10 +0100

    move actie form to higher scope

--- js/app.js
+++ js/app.js
@@ -29,9 +29,52 @@ WhatNowApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/open');
 })
 
-.controller('WhatNowCtrl', function($scope, firebaseService, $state){
+.controller('WhatNowCtrl', function($scope, firebaseService, activityFactory, $ionicModal, $state, $stateParams){
     $scope.activities = firebaseService.activities;
     $scope.users = firebaseService.users;
+
+    //Add or Edit an Activity Modal, refactor into a service?
+    $ionicModal.fromTemplateUrl('templates/form-activity.html', function(modal){
+        $scope.formModal = modal;
+    },{
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+
+    $scope.openActivityForm = function(){
+
+        $scope.forUsers = [];
+        //are we creating or editing an activity?
+        if($stateParams.activityId){ //can only edit in single activity url
+            $scope.editMode = true;
+            var id = $stateParams.activityId;
+            $scope.activity = firebaseService.activities[id];
+            angular.forEach($scope.activity.users, function(user){
+                $scope.forUsers[user] = true;
+            });
+        }else { //add new activity
+            $scope.editMode = false;
+            $scope.activity = activityFactory.newActivity();
+        }
+
+        $scope.formModal.show();
+
+        $scope.addEditActivity = function () {
+            var activity = (activityFactory.prep($scope.activity));
+            if(!$scope.editMode) { //add new activity
+                firebaseService.add(activity);
+            }else { //edit one based on url
+                firebaseService.activities[id] = $scope.activity;
+//                firebaseService.add(activity); //TEMP for rebuilding database
+                firebaseService.activities.$save(id);
+            }
+            $scope.formModal.hide();
+        };
+
+        $scope.toggleUser = function(user){
+            $scope.activity.users = activityFactory.toggleInArray($scope.forUsers, user);
+        };
+    };
 
     $scope.goToActivity = function(activityId){
         $state.go("activity", {activityId: activityId});
